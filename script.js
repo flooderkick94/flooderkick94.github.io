3007d0cc3ccfc023d77fa8b7e82485f6d2bcc2a6
flooderkick94 <57605192+flooderkick94@users.noreply.github.com>
2022-06-11 15:44:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const tags = document.querySelectorAll('[data-tab-target]')
-const tabConetents = document.querSelectorAll('[data-tab-content]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.tabTarget)
